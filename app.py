c9b39610e71d9771d58a5829a8e5b98181fd4221
COOL3163 <85270012+COOL3163@users.noreply.github.com>
2025-04-02 07:23:28 +0000

    fix everything

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from sustainability_game import SustainabilityGame
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Required for session management
+app.secret_key = 'your_secret_key'
 
 # Initialize the game
 game = SustainabilityGame()
@@ -16,12 +16,18 @@ def start_game():
     player_name = request.form.get('player_name')
     if not player_name:
         return redirect(url_for('home'))
-    session['player_name'] = player_name
+
+    global game
+    game = SustainabilityGame()
     game.player_name = player_name
+    
     return redirect(url_for('game_view'))
 
 @app.route('/game')
 def game_view():
+    if game.days > 7:  # End the game after 7 days
+        return redirect(url_for('game_over'))
+
     player = {
         "name": game.player_name,
         "eco_points": game.eco_points,
@@ -30,16 +36,28 @@ def game_view():
         "sustainability": game.sustainability_level,
         "location": game.current_location
     }
+    location = game.locations[game.current_location]
     return render_template(
         'game.html',
         player=player,
-        actions=game.locations[game.current_location]['actions']
+        location=location,
+        actions=location['actions'],
+        game=game,  # Pass the game object to the template
+        tip=session.pop('sustainability_tip', None)
     )
 
 @app.route('/action/<action>')
 def perform_action(action):
-    if action in game.locations[game.current_location]['actions']:
-        game.perform_action(action)
+    result = game.perform_action(action)
+    if "error" in result:
+        return redirect(url_for('game_view'))
+    return redirect(url_for('game_view'))
+
+@app.route('/change_location/<location>')
+def change_location(location):
+    if location in game.locations:
+        game.current_location = location
+        game.energy -= 5  # Traveling consumes energy
     return redirect(url_for('game_view'))
 
 @app.route('/end_day')
@@ -47,15 +65,22 @@ def end_day():
     game.end_day()
     return redirect(url_for('game_view'))
 
-@app.route('/change_location/<location>')
-def change_location(location):
-    if location in game.locations:
-        game.current_location = location
+@app.route('/random_tip')
+def random_tip():
+    session['sustainability_tip'] = game.get_random_tip()
     return redirect(url_for('game_view'))
 
-@app.route('/quit')
-def quit_game():
-    return redirect(url_for('home'))
+@app.route('/game_over')
+def game_over():
+    return render_template(
+        'game_over.html',
+        stats={
+            "name": game.player_name,
+            "days": game.days,
+            "eco_points": game.eco_points,
+            "sustainability": game.sustainability_level
+        }
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
